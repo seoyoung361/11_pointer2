7123b5246b07be12deddb8353524356a2451ca1d
seoyoung361 <chris7400@naver.com>
2019-11-14 15:30:43 +0900

    04

--- main.c
+++ main.c
@@ -5,18 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	char buffer[8];
-	double *pd;
-	int *pi;
-	
-	pd=(double*)buffer;
-	*pd=3.14;
-	printf("%f\n", *(double*)buffer);
-	
-	pi=(int*)buffer;
-	*pi=123;
-	*(pi+1)=456;
-	printf("%d %d\n", *(int*)buffer, *((int*)buffer+1));
-	
+ int i=100;
+ int *p=&i;
+ int **q=&p;
+
+ *p=200;
+ printf("i=%d, *p=%d, **q=%d\n", i, *p, **q);
+
+ **q=300;
+ printf("i=%d, *p=%d, **q=%d\n" , i, *p, **q);
+
 	return 0;
 }
